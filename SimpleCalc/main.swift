8675b46bed33c665f1b1debee07818c58db1dd20
yiying song <Yiying>
2016-04-05 10:39:22 -0700

    first

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,76 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by apple on 4/3/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+import Foundation
+
+func input() -> String {
+    let keyboard = NSFileHandle.fileHandleWithStandardInput()
+    let inputData = keyboard.availableData
+    let result = NSString(data: inputData, encoding:NSUTF8StringEncoding)
+        as! String
+    return result.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+}
+
+let firstInput = input()
+let whitespace = NSCharacterSet.whitespaceCharacterSet()
+let range = firstInput.rangeOfCharacterFromSet(whitespace)
+
+
+if let test = range {
+    
+    let separated = firstInput.componentsSeparatedByString(" ")
+    if separated.last == "count" {
+        print(separated.count - 1)
+    } else if separated.last == "avg" {
+        var sum : Int? = 0
+        for index in 0...separated.count-2 {
+            sum = sum! + Int(separated[index])!
+        }
+        var length = separated.count - 1
+        print(sum! / length)
+    } else {
+        var last : Int? = Int(separated[0])
+        func factorial(last: Int) -> (Int) {
+            if (last <= 1) {
+                return 1
+            } else {
+                return last * factorial(last - 1)
+            }
+        }
+        print(factorial(last!))
+    }
+}
+else {
+    print("Enter an expression separated by returns")
+
+    var firstNum : Int? = Int(firstInput)
+    var symbol = input()
+    var secondNum : Int? = Int(input())
+    var result : Int
+
+
+    if symbol == "+" {
+        result = firstNum! + secondNum!
+    } else if symbol == "-"{
+        result = firstNum! - secondNum!
+    } else if symbol == "*" {
+        result = firstNum! * secondNum!
+    } else if symbol == "/" {
+        result = firstNum! / secondNum!
+    } else {
+        result = firstNum! % secondNum!
+    }
+
+    print("Result: \(result)")
+}
+
+
+
+
+
+
